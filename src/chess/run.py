845ae1db71db0bc6606d26a31faf4917473b9332
henrike <henrikept01@gmail.com>
2023-05-15 22:51:07 +0100

    commit

--- src/chess/run.py
+++ src/chess/run.py
@@ -3,20 +3,6 @@ from chess.player.playerRandom import PlayerRandom
 from chess.sim.simulatorPVP import SimulatorPVP
 from chess.sim.visualizer import Visualizer
 
-# # init player adb
-# adb = start_abd()
-# player_adb = PlayerADB(adb)
-# game = Game()
-#
-# # init other player
-# player_random = PlayerRandom()
-#
-# player_w = player_adb if player_adb.is_white else player_random
-# player_b = player_random if player_adb.is_white else player_adb
-#
-# simulator = Simulator(game, player_w, player_b)
-# simulator.execute()
-
 game = ChessGame()
 player_w = PlayerRandom()
 player_b = PlayerRandom()
