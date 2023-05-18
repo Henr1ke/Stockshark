9c4b652d4e0623e394302857ce206f64b21c91d7
Henrique <henrikept01@gmail.com>
2023-05-18 15:06:19 +0100

    commit

--- src/chess/piece/king.py
+++ src/chess/piece/king.py
@@ -8,7 +8,6 @@ class King(Piece):
 
     def gen_positions(self, game) -> List[Position]:
         board = game.board
-
         start_pos = self.get_pos(board)
 
         increments = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
@@ -16,10 +15,11 @@ class King(Piece):
 
         # Adds castling positions to the possible positions if possible
         can_castle_q = ("Q" if self.is_white else "q") in game.castlings
-        can_castle_k = ("K" if self.is_white else "k") in game.castlings
         if can_castle_q and board[1, start_pos.row] is None and board[2, start_pos.row] is None \
                 and board[3, start_pos.row] is None:
             positions.append(start_pos + (-2, 0))
+
+        can_castle_k = ("K" if self.is_white else "k") in game.castlings
         if can_castle_k and board[5, start_pos.row] is None and board[6, start_pos.row] is None:
             positions.append(start_pos + (2, 0))
 
