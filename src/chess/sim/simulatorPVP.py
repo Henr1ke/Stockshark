6c25993af8a2047c47646f1046147272f17685c8
Henrique <henrikept01@gmail.com>
2023-05-19 15:22:37 +0100

    commit

--- src/chess/sim/simulatorPVP.py
+++ src/chess/sim/simulatorPVP.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from chess.chessGame.chessGame import ChessGame
 from chess.player.player import Player
+from chess.player.playerRandom import PlayerRandom
 from chess.sim.simulator import Simulator
 from chess.sim.visualizer import Visualizer
 
@@ -17,3 +18,13 @@ class SimulatorPVP(Simulator, ABC):
         player = self._player_w if self._game.is_white_turn else self._player_b
         move = player.gen_move(self._game)
         self._game.play(move)
+
+
+if __name__ == '__main__':
+    game = ChessGame()
+    player_w = PlayerRandom()
+    player_b = PlayerRandom()
+    visualizer = Visualizer(Visualizer.W_PIECE_CHARSET_LETTER, Visualizer.B_PIECE_CHARSET_LETTER)
+
+    simulator = SimulatorPVP(player_w, player_b, game, visualizer)
+    simulator.execute()
