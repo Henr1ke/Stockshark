28657e40d9bc5413193361b52270aaa36f24c1ee
Henrique <henrikept01@gmail.com>
2023-06-20 16:49:07 +0100

    commit

--- src/stockshark/chess_engine/stockfish_dao.py
+++ src/stockshark/chess_engine/stockfish_dao.py
@@ -1,34 +1,43 @@
-import threading
-import subprocess
+from threading import Thread
+from subprocess import Popen, PIPE, STDOUT
 from queue import Queue, Empty
-import time
-from typing import List, Optional
+from typing import Optional
 
 
 class StockFishDao:
     def __init__(self, path: str = "../../stockfish/stockfish-windows-2022-x86-64-modern.exe") -> None:
 
         self._path = path
-        self._stockfish = subprocess.Popen(
+        self._stockfish = Popen(
             self._path,
             universal_newlines=True,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=STDOUT,
         )
 
         self._sf_output = Queue()
 
-        self._line_reader = threading.Thread(target=StockFishDao._thread_read_contents,
-                                             args=(self._stockfish, self._sf_output,),
-                                             daemon=True)
+        def _thread_read_contents(sf_process: Popen, sf_output: Queue) -> None:
+            while True:
+                if not sf_process.stdout:
+                    raise BrokenPipeError()
+                if sf_process.poll() is not None:
+                    raise ConnectionError("The Stockfish process has crashed")
+                line = sf_process.stdout.readline().strip()
+                sf_output.put(line)
+        self._line_reader = Thread(target=_thread_read_contents,
+                                   args=(self._stockfish, self._sf_output,),
+                                   daemon=True)
         self._line_reader.start()
+        self._read_line()  # Clear Queue
 
         self._is_closing = False
 
         self._send_command("uci")
         response = self._read_response()
-        print(response)
+        if "uciok" not in response:
+            raise ChildProcessError('No "uciok" message recieved from stockfish')
 
     def _send_command(self, command: str) -> None:
         if not self._stockfish.stdin:
@@ -38,33 +47,26 @@ class StockFishDao:
             self._stockfish.stdin.flush()
             if command == "quit":
                 self._is_closing = True
-            time.sleep(0.1)
 
-    def _read_line(self) -> Optional[str]:
+    def _read_line(self, timeout: float = 0.1) -> Optional[str]:
         try:
-            line = self._sf_output.get_nowait()
-            # line = self._sf_output.get(timeout=0.1)
+            line = self._sf_output.get(timeout=timeout)
             return line
         except Empty:
             return None
 
-    def _read_response(self) -> Optional[str]:
+    def _read_response(self, timeout: float = 0.1) -> Optional[str]:
         lines = []
-        line = self._read_line()
+        line = self._read_line(timeout)
         while line is not None:
             lines.append(line)
             line = self._read_line()
         return "\n".join(lines)
 
-    @staticmethod
-    def _thread_read_contents(sf_process: subprocess.Popen, sf_output: Queue) -> None:
-        while True:
-            if not sf_process.stdout:
-                raise BrokenPipeError()
-            if sf_process.poll() is not None:
-                raise ConnectionError("The Stockfish process has crashed")
-            line = sf_process.stdout.readline().strip()
-            sf_output.put(line)
+    def _is_ready(self) -> None:
+        self._put("isready")
+        while self._read_line() != "readyok":
+            pass
 
     def close(self):
         self._send_command("quit")
