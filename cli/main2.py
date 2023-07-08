c66cef9110966d306ab7d0e7ba4cb0c88b46667a
Henrique <henrikept01@gmail.com>
2023-07-08 19:13:23 +0100

    commit

--- cli/main2.py
+++ cli/main2.py
@@ -1,97 +1,64 @@
 import argparse
-from typing import Optional
 
-from stockshark.adb.coordinates.coordinates import Coordinates
 from stockshark.adb.coordinates.coordinates_mi_8_lite import CoordinatesMi8Lite
 from stockshark.adb.coordinates.coordinates_pixel_3 import CoordinatesPixel3
 from stockshark.adb.coordinates.coordinates_pixel_4 import CoordinatesPixel4
-from stockshark.agent.agent import Agent
 from stockshark.agent.agent_human import AgentHuman
-from stockshark.agent.agent_min_max import AgentMinMax
+from stockshark.agent.agent_min_max_ab import AgentMinMaxAB
 from stockshark.agent.agent_random import AgentRandom
 from stockshark.agent.agent_reactive import AgentReactive
-from stockshark.run.stockshark_runnable import StockSharkRunnable
-
+from stockshark.chess_engine.python_chess_engine import PythonChessEngine
+from stockshark.chess_engine.stockshark_engine import StocksharkEngine
 
 # adb start-server
 # emulator -avd Pixel_4_API_33 -port 5556
 
-# python main.py --agent reactive --model pixel4 friend --username Henrike01
-# python main.py --agent minmax --model pixel4 computer --diff_lvl 1
+models_dict = {"pixel4": CoordinatesPixel4, "pixel3": CoordinatesPixel3, "mi8lite": CoordinatesMi8Lite}
+engines_dict = {"python_chess": PythonChessEngine, "stockshark": StocksharkEngine()}
+agents_dict = {"human": AgentHuman, "random": AgentRandom, "reactive": AgentReactive, "minmax": AgentMinMaxAB}
 
-def get_coordinates(model: str) -> Optional[Coordinates]:
-    if model.casefold() == "pixel3".casefold():
-        return CoordinatesPixel3()
-    if model.casefold() == "pixel4".casefold():
-        return CoordinatesPixel4()
-    if model.casefold() == "mi8lite".casefold():
-        return CoordinatesMi8Lite()
-    return None
+main_parser = argparse.ArgumentParser(description="StockShark is a chess engine that plays chess on the chess.com app "
+                                                  "using ADB to control the smartphone. It can play against a human "
+                                                  "friend or against the chess.com computer. The chess.com app "
+                                                  "navigation throught the menus to start a game can be automated or "
+                                                  "the user can navigate manually.")
 
-def get_agent(agent: str) -> Optional[Agent]:
-    if agent.casefold() == "human".casefold():
-        return AgentHuman()
-    if agent.casefold() == "random".casefold():
-        return AgentRandom()
-    if agent.casefold() == "reactive".casefold():
-        return AgentReactive()
-    if agent.casefold() == "minmax".casefold():
-        return AgentMinMax()
-    return None
+main_parser.add_argument('--model', choices=models_dict.keys(), required=True,
+                         help='Which smartphone model the ADB connects to')
+main_parser.add_argument('--engine', choices=engines_dict.keys(), required=True, help='Which chess engine to use')
+main_parser.add_argument('--agent', choices=agents_dict.keys(), required=True,
+                         help="Which agent to run on the simulation")
 
+nav_mode_subparsers = main_parser.add_subparsers(dest='menu_navigation_mode', required=True)
 
-def get_paw_response(play_as_whites: str) -> Optional[bool]:
-    if play_as_whites is None:
-        return None
-    return play_as_whites.casefold() == "y".casefold() or play_as_whites.casefold() == "yes".casefold() or \
-        play_as_whites.casefold() == "s".casefold() or play_as_whites.casefold() == "sim".casefold() or \
-        play_as_whites.casefold() == "true".casefold()
+menu_nav_manual_parser = nav_mode_subparsers.add_parser('menu_nav_manual',
+                                                        help='The program assumes that the user has already '
+                                                             'navigated throught chess.com menus to start a '
+                                                             'game and it will only play the game')
+menu_nav_automatic_parser = nav_mode_subparsers.add_parser('menu_nav_automatic',
+                                                           help='The program automatically navigates '
+                                                                'throught chess.com menus to start a game '
+                                                                'and then plays the game')
 
+opponent_type_subparsers = menu_nav_automatic_parser.add_subparsers(dest='oppenent_type', required=True)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--agent', type=str, required=True, help="Which agent model to run on the simulation")
-parser.add_argument('--model', type=str, required=True, help='Which smartphone model the ADB connects to')
-subparser = parser.add_subparsers(
-    dest='opponent_type', required=True,
-    help="Insert 'friend' to play against another friend or 'computer' to play against a computer")
+vs_friend_parser = opponent_type_subparsers.add_parser('vs_friend', help='Play against a human friend')
+vs_friend_parser.add_argument('--username', required=True, help='The username of the friend')
+vs_firend_color_group = vs_friend_parser.add_mutually_exclusive_group(required=False)
+vs_firend_color_group.add_argument('--play_as_whites', action='store_true', help='Play with the white pieces')
+vs_firend_color_group.add_argument('--play_as_blacks', action='store_true', help='Play with the black pieces')
+vs_friend_parser.add_argument('--player_timers', choices=[1, 3, 5, 10, 30], default=10, help='The timer duration, in '
+                                                                                             'minutes, that each '
+                                                                                             'player has to act during '
+                                                                                             'the whole match')
 
-friend = subparser.add_parser('friend')
-friend.add_argument("--username", type=str, required=True)
-friend.add_argument("--play_as_whites", type=str, required=False)
-friend.add_argument("--duration", type=int, required=False)
+vs_computer_parser = opponent_type_subparsers.add_parser('vs_computer', help='Play against the chess.com computer')
+vs_computer_parser.add_argument('--diff_lvl', choices=[1, 2, 3, 4, 5], default=1, help='The difficulty level of the '
+                                                                                       'chess.com coputer')
+vs_computer_color_group = vs_computer_parser.add_mutually_exclusive_group(required=False)
+vs_computer_color_group.add_argument('--play_as_whites', action='store_true', help='Play with the white pieces')
+vs_computer_color_group.add_argument('--play_as_blacks', action='store_true', help='Play with the black pieces')
 
-computer = subparser.add_parser('computer')
-computer.add_argument("--diff_lvl", type=int, required=True)
-computer.add_argument("--play_as_whites", type=str, required=False)
+args = main_parser.parse_args()
 
-args = parser.parse_args()
-
-agent = get_agent(args.agent)
-if agent is None:
-    raise ValueError("The agent type is not recognized")
-
-coordinates = get_coordinates(args.model)
-if coordinates is None:
-    raise ValueError("The model is not available to playtest")
-
-print("--- Starting STOCKSHARK ---")
-stockshark = StockSharkRunnable(coordinates)
-
-stockshark.open_app(8)
-print("Opening app")
-
-vs_bot = args.opponent_type.casefold() == "computer"
-if vs_bot:
-    diff_lvl = args.diff_lvl
-    on_white_side = get_paw_response(args.play_as_whites)
-    print("Starting a game against the computer")
-    stockshark.start_game_computer(diff_lvl, on_white_side, 3)
-else:
-    username = args.username
-    on_white_side = get_paw_response(args.play_as_whites)
-    duration = args.duration
-    print("Starting a game against a friend")
-    stockshark.start_game_friend(username, on_white_side, duration, 3)
-
-print("Playing the game")
-stockshark.run_game(agent)
+print(args)
